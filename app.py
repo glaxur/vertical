3884474bf1e26c7f95fcf9bcb8043efb3cc647bc
isaiah santillan <isaiahsantillan111@icloud.com>
2019-01-11 11:55:09 -0500

    updated style sheet and python for refresh

--- app.py
+++ app.py
@@ -6,22 +6,27 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 
-    input = request.args.get('search-term')
-    print(input)
+   input = request.args.get("position_right")
+   print(input)
 
-    # open file for reading, 'r'
-    # file is saved to variable
-    index_file = open('index.html', 'r')
-    # read contents of the file
-    my_html = index_file.read()
+   position_left = "left"
+   position_right = "right"
 
-    if input:
-        # add use input back into the html
-        my_html = my_html.replace("{{search-term-value}}",input)
-    else:
-        my_html = my_html.replace("{{search-term-value}}","")
+   # open file for reading, 'r'
+   # file is saved to variable
+   index_file = open('index.html', 'r')
+   # read contents of the file
+   my_html = index_file.read()
 
-    # close the file out when you're done
-    index_file.close()
+   if input:
+       # add use input back into the html
+       my_html = my_html.replace("{{position_left}}", position_left)
+       my_html = my_html.replace("{{position_right}}", position_right)
+   else:
+       my_html = my_html.replace("{{position_right}}", position_left)
+       my_html = my_html.replace("{{position_left}}", position_right)
 
-    return my_html
+   # close the file out when you're done
+   index_file.close()
+
+   return my_html
