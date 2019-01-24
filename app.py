0aead209bb60f0bc4fb23b3f996f32b7db29d2ad
Isaiah Santillan <isaiahsantillan111@icloud.com>
2019-01-24 13:32:27 -0500

    pep8 app.py

--- app.py
+++ app.py
@@ -3,30 +3,30 @@ from flask import request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
 
-   input = request.args.get("position_right")
-   print(input)
+    input = request.args.get("position_right")
+    print(input)
 
-   position_left = "left"
-   position_right = "right"
+    position_left = "left"
+    position_right = "right"
 
-   # open file for reading, 'r'
-   # file is saved to variable
-   index_file = open('index.html', 'r')
-   # read contents of the file
-   my_html = index_file.read()
+    # open file for reading, 'r'
+    # file is saved to variable
+    index_file = open('index.html', 'r')
+    # read contents of the file
+    my_html = index_file.read()
 
-   if input:
-       # add use input back into the html
-       my_html = my_html.replace("{{position_left}}", position_left)
-       my_html = my_html.replace("{{position_right}}", position_right)
-   else:
-       my_html = my_html.replace("{{position_right}}", position_left)
-       my_html = my_html.replace("{{position_left}}", position_right)
+    if input:
+        # add use input back into the html
+        my_html = my_html.replace("{{position_left}}", position_left)
+        my_html = my_html.replace("{{position_right}}", position_right)
+    else:
+        my_html = my_html.replace("{{position_right}}", position_left)
+        my_html = my_html.replace("{{position_left}}", position_right)
+# close the file out when you're done
+    index_file.close()
 
-   # close the file out when you're done
-   index_file.close()
-
-   return my_html
+    return my_html
